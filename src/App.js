efdba1f94e47c9aa9c3a15551c1ee268e8c7c4b2
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-12-01 12:13:26 +0530

    added styled components and removed tailwind and daisy ui as i found issues to remeber all their classnames

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
+import styled from 'styled-components';
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className='alert alert-error shadow-lg'>
-      <div>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          className='stroke-current flex-shrink-0 h-6 w-6'
-          fill='none'
-          viewBox='0 0 24 24'
-        >
-          <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            strokeWidth='2'
-            d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
-          />
-        </svg>
-        <span>Error! Task failed successfully.</span>
-      </div>
-    </div>
+    <>
+      <HomeContain>
+        <Navbar title='GitHub' />
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </HomeContain>
+    </>
   );
 }
 
 export default App;
+
+const HomeContain = styled.div`
+  height: 100vh;
+  width: 100vw;
+  background-color: #767676;
+`;
---
+++ src/components/Navbar.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import { FaGithub } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import { PropTypes } from 'prop-types';
+import styled from 'styled-components';
+
+function Navbar({ title }) {
+  return (
+    <NavContain>
+      <NavTitle>
+        <Link to='/'>
+          <FaGithub />
+        </Link>
+        <Link to='/'>{title}</Link>
+      </NavTitle>
+      <NavLinks>
+        <Link to='/'>Home</Link>
+        <Link to='/about'>About</Link>
+      </NavLinks>
+    </NavContain>
+  );
+}
+
+Navbar.defaultProps = {
+  title: 'GithubFinder',
+  icon: <FaGithub />,
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.element.isRequired,
+};
+
+export default Navbar;
+
+const NavContain = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem 1rem;
+  background-color: #00e773;
+  color: #fff;
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
+
+const NavTitle = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.4rem;
+  color: white;
+  a {
+    color: white;
+    font-size: 1.2rem;
+    svg {
+      display: flex;
+      align-items: center;
+      font-size: 2rem;
+    }
+  }
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.5rem;
+  a {
+    color: #fff;
+    text-decoration: none;
+    padding: 0.5rem 1rem;
+    box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+    border-radius: 5px;
+    font-size: 1rem;
+    &:hover {
+      text-decoration: none;
+      background-color: #14ca6c;
+    }
+  }
+`;
