3180c195f887369f0cdbeef2a9f7dbdbe6288578
Ayush Pandit <ayushpanditmoto@gmail.com>
2022-12-01 09:43:06 +0530

    added tailwind and daisy ui plugins

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='alert alert-error shadow-lg'>
+      <div>
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='stroke-current flex-shrink-0 h-6 w-6'
+          fill='none'
+          viewBox='0 0 24 24'
         >
-          Learn React
-        </a>
-      </header>
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+          />
+        </svg>
+        <span>Error! Task failed successfully.</span>
+      </div>
     </div>
   );
 }
